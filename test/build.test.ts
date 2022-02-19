46722dad9b542433b194969906fa1b5dcae93da4
murao@shanon <murao@shanon.co.jp>
2022-02-19 12:01:07 +0900

    ファイル出力のテスト追加

--- test/build.test.ts
+++ test/build.test.ts
@@ -16,7 +16,31 @@ describe('build', () => {
   });
   test('ファイル出力', async () => {
     const outputDir = resolve(__dirname, '../dist/');
-    const quizembedFileName = await build(outputDir);
+    const quizembedFileName = await build(outputDir, {
+      answer: {
+        correct: {
+          no: '1',
+          msg: '正解です',
+        },
+        wrong: {
+          msg: '不正解です',
+        },
+        answerText: '正解は:1番です',
+        comment:
+          'クイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説ですクイズの解説です',
+      },
+      question: {
+        headerText: 'クイズです',
+        qtext:
+          'なんでしょうか？なんでしょうか？なんでしょうか？なんでしょうか？なんでしょうか？なんでしょうか？なんでしょうか？なんでしょうか？なんでしょうか？なんでしょうか？なんでしょうか？なんでしょうか？なんでしょうか？なんでしょうか？',
+        qimagePath: './onigarayaki.jpg',
+      },
+      choices: [
+        {no: '1', text: '選択肢1です'},
+        {no: '2', text: '選択肢2です。'},
+        {no: '3', text: '選択肢3です。'},
+      ],
+    });
     const fileFullPath = `${outputDir}/${quizembedFileName}`;
 
     outputPaths.push(fileFullPath);
